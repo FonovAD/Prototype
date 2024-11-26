032923610c91513cc51aecfe2a66e71975088247
Alexandr Fonov <FonovAD@Gmail.com>
2024-11-26 21:41:43 +0300

    feature logging middleware

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,1 +1,32 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (s *server) Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		lrw := NewLoggingResponseWriter(w)
+		next.ServeHTTP(lrw, req)
+		s.metricMonitor.IncRequestsTotal(req.Method, lrw.statusCode)
+		s.metricMonitor.IncRequestLatency(req.Method, req.URL.Path, time.Since(start).Seconds())
+		fmt.Println(w.Header()["status_code"])
+	})
+}
