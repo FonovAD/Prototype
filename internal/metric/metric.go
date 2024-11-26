5f46814e06bd575d895c811c7b2436ab934b090f
Alexandr Fonov <FonovAD@Gmail.com>
2024-11-26 20:13:01 +0300

    feature package metric
    The monitor initialization method, basic metrics
    and their increment functions have been added.

---
+++ internal/metric/metric.go
@@ -0,0 +1,55 @@
+package metric
+
+import (
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type MetricMonitor struct {
+	requestsTotal  *prometheus.CounterVec
+	requestLatency *prometheus.HistogramVec
+	errorCount     *prometheus.CounterVec
+}
+
+func New() *MetricMonitor {
+	requestsTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_requests_total",
+			Help: "Total number of requests",
+		},
+		[]string{"method", "status"})
+	requestLatency := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Latency of HTTP requests in seconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+	errorCount := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_errors_total",
+			Help: "Total number of HTTP error responses",
+		},
+		[]string{"method", "path", "status"},
+	)
+	return &MetricMonitor{
+		requestsTotal:  requestsTotal,
+		requestLatency: requestLatency,
+		errorCount:     errorCount,
+	}
+}
+
+func (m *MetricMonitor) IncRequestsTotal(method, status string) {
+	m.requestsTotal.WithLabelValues(method, status).Inc()
+}
+
+func (m *MetricMonitor) IncRequestLatency(method, path string, duration float64) {
+	m.requestLatency.WithLabelValues(method, path).Observe(duration)
+}
+
+func (m *MetricMonitor) IncErrorCount(method, path string, status int) {
+	statusGroup := strconv.Itoa(status % 100)
+	m.errorCount.WithLabelValues(method, path, statusGroup).Inc()
+}
