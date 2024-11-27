5d56f7e025eed4face567a5747adb58aa3e1cd23
Alexandr Fonov <FonovAD@Gmail.com>
2024-11-27 21:41:49 +0300

    chore []models.Link{} -> []models.Link

--- internal/store/SQLstore/linkrepository.go
+++ internal/store/SQLstore/linkrepository.go
@@ -1,6 +1,10 @@
 package sqlstore
 
-import "context"
+import (
+	"context"
+
+	"github.com/FonovAD/Prototype/internal/models"
+)
 
 type LinkRepository struct {
 	store *Store
@@ -10,10 +14,10 @@ func (l *LinkRepository) Create(ctx context.Context) (models.Link, error) {
 	return models.Link{}, nil
 }
 func (l *LinkRepository) GetByUID(ctx context.Context, uid int) ([]models.Link, error) {
-	return make([]models.Link{}, 5), nil
+	return make([]models.Link, 5), nil
 }
 func (l *LinkRepository) GetByOriginLink(ctx context.Context, originLink string) ([]models.Link, error) {
-	return make([]models.Link{}, 5), nil
+	return make([]models.Link, 5), nil
 }
 func (l *LinkRepository) Delete(ctx context.Context, originLink string) error {
 	return nil
