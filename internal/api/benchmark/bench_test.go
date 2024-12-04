ddf68b71c625ba9730a2f8aa0397b289c6b61714
Alexandr Fonov <FonovAD@Gmail.com>
2024-12-04 23:08:43 +0300

    chore: add trace for benchmarks

--- internal/api/benchmark/bench_test.go
+++ internal/api/benchmark/bench_test.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"runtime/trace"
 	"testing"
 	"time"
 
@@ -36,6 +38,28 @@ func BenchmarkServer_Hello(b *testing.B) {
 	}
 }
 
+func BenchmarkServer_CreateUser(b *testing.B) {
+	f, err := os.Create("trace_benchmark.out")
+	if err != nil {
+		b.Fatalf("Error creating trace file: %v", err)
+	}
+	defer f.Close()
+
+	if err := trace.Start(f); err != nil {
+		b.Fatalf("Error starting trace: %v", err)
+	}
+	defer trace.Stop()
+
+	s := api.NewServer(logger.New("debug"), metric.NewTest(), SetupDB())
+	rec := httptest.NewRecorder()
+
+	for i := 0; i < b.N; i++ {
+		req, _ := http.NewRequest(http.MethodPost, "/create_user", nil)
+		req.Header.Set("Authorization", "token test")
+		s.ServeHTTP(rec, req)
+	}
+}
+
 func SetupDB() store.Store {
 	databasePath := "./LinkShortener"
 	db, err := sql.Open("sqlite3", databasePath)
@@ -58,7 +82,10 @@ CreatedAt integer,
 ExpirationTime integer NOT NULL,
 Status varchar(10) NOT NULL,
 ScheduledDeletionTime integer NOT NULL
-);`
+);
+
+INSERT INTO users(Token, Role) VALUES("test", "admin");
+`
 	if _, err := db.Exec(schema); err != nil {
 		log.Fatalf("Failed to setup test database schema: %v", err)
 	}
