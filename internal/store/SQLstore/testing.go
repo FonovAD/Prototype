1072d529e9984bb6bf5e211be2888496f1c3a32f
Alexandr Fonov <FonovAD@Gmail.com>
2024-12-12 20:01:39 +0300

    fix: relation links
    A composite primary key has been added for the
    "links" relation. In addition, the "Create" method
     in the linkrepository.go file has been updated.
     Also, the database schema is now stored in one
     place - sqlstore.

--- internal/store/SQLstore/testing.go
+++ internal/store/SQLstore/testing.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"testing"
 
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -39,27 +38,7 @@ func SetupTestDB(t *testing.T, tokenForAdmin string) (*sql.DB, func(...string)) 
 	if err != nil {
 		t.Fatalf("Failed to connect to database: %v", err)
 	}
-	schema := `
-PRAGMA foreign_keys = ON;
-CREATE TABLE IF NOT EXISTS users(
-UID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-Token TEXT NOT NULL,
-Role varchar(10) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS links(
-UID INTEGER REFERENCES users(UID) ON DELETE CASCADE,
-OriginLink TEXT UNIQUE NOT NULL,
-ShortLink TEXT UNIQUE NOT NULL,
-CreatedAt integer,
-ExpirationTime integer NOT NULL,
-Status varchar(10) NOT NULL,
-ScheduledDeletionTime integer NOT NULL
-);
-
-INSERT INTO users(Token, Role) VALUES("%s", "admin");
-`
-	if _, err := db.Exec(fmt.Sprintf(schema, tokenForAdmin)); err != nil {
+	if _, err := db.Exec(fmt.Sprintf(Schema, tokenForAdmin)); err != nil {
 		t.Fatalf("Failed to setup test database schema: %v", err)
 	}
 	return db, func(tables ...string) {
